e7147048ba17e06e0a7b5f3bffe27fe9e1469495
Tezigudo <80502090+Tezigudo@users.noreply.github.com>
2023-03-18 09:25:55 +0700

    🏗️  added card model

---
+++ src/models/Card.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const Card = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    value: {
+        type: Number,
+        default: 0
+    },
+    description: {
+        type: String,
+        default: ""
+
+    },
+    matchHistory: {
+        type: Array,
+        default: []
+    }
+});
+
+module.exports = Card = mongoose.model('card', Card);
