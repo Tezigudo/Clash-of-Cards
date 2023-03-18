e31ba248ba1a12177832eeb1a9f3c1b10f79522a
Tezigudo <80502090+Tezigudo@users.noreply.github.com>
2023-03-18 09:29:34 +0700

    🏗️  added card model

---
+++ src/models/Deck.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const DeckSchema = new Schema({
+    card: {
+        type: Array,
+        default: []
+    }
+});
+
+// DeckSchema.methods.addCard = function (card) {
+//     this.card.push(card);
+//     return this.save();
+// }
+
+module.exports = Deck = mongoose.model('player', DeckSchema);
