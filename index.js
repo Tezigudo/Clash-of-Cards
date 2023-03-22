a45c63ea34f8cfee4c11c13aa87a05cf11fe484c
Tezigudo <80502090+Tezigudo@users.noreply.github.com>
2023-03-22 11:24:57 +0700

    🎨 remove comment

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
 "use strict";
 
 require('dotenv').config();
-// const express = require('express');
 const mongoose = require('mongoose');
-// const cookieParser = require('cookie-parser');
-// const app = express(); // Create an express app
-// const cors = require('cors');
-// const SessionMiddleware = require('./src/middlewares/session');
 
 const app = require('./src/app');
 
@@ -19,9 +14,7 @@ const io = require('socket.io')(server, {
     }
 }); // Create a socket.io server
 const socketioJwt = require('socketio-jwt'); // Import socketio-jwt
-// const { loginRequired, logoutRequired, verifyToken } = require('./src/middlewares/auth');
-const Player = require('./src/models/Player');
-const helmet = require('helmet');
+
 
 io.use(socketioJwt.authorize({
     secret: process.env.SECRET,
@@ -33,22 +26,9 @@ io.use(socketioJwt.authorize({
 
 const socketRouter = require('./src/routes/SocketRouter')(io); // Import the SocketRouter
 
-// app.use(SessionMiddleware)
-
-// app.use(cookieParser())
-// app.use(helmet())
-// app.use(verifyToken)
-
-
-// app.use(cors({
-//     origin: "*",
-// }))
 
 
 app.use('/api', socketRouter); // Use the SocketRouter (api/
-
-// app.set("view engine", "ejs"); // Set the view engine to ejs
-// app.set('views', __dirname + '/src/views'); // Set the views folder to src/views
 
 mongoose.connect(process.env.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true }).then((res) => {
     console.log(`Connected to database ${res}`)
@@ -59,31 +39,6 @@ mongoose.connect(process.env.mongoURI, { useNewUrlParser: true, useUnifiedTopolo
 server.listen(3000, () => {
     console.log("Server is running on port 3000");
 });
-
-// // Set the body parser to urlencoded (for forms and stuff)
-// app.use(express.urlencoded({ extended: true }));
-
-// app.use(express.json()) // Set the body parser to json
-
-// app.use('/user', require('./src/routes/UserRouter')); // Use the UserRouter (api/user/)
-
-// // Render the index page
-// app.get('/', loginRequired, verifyToken, (req, res) => {
-//     res.locals.token = req.cookies.token;
-//     res.render("index", { name: req.payload.name, id: req.payload.id });
-// });
-
-// // Render the login page
-// app.get('/login', logoutRequired, (req, res) => {
-//     res.render("login", { message: "" });
-// })
-
-// // Render the register page
-// app.get('/register', (req, res) => {
-//     res.render("register");
-// })
-
-
 
 // socket.io stuff
 io.on('connection', (socket) => {
