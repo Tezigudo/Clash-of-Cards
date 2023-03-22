26f43841ea3319804150240fbe9a618dc6a83670
Tezigudo <80502090+Tezigudo@users.noreply.github.com>
2023-03-22 12:11:40 +0700

    🎨 move server in index.js to server.js

--- index.js
+++ index.js
@@ -4,29 +4,10 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 
 const app = require('./src/app');
+const server = require('./server');
 
-
-const server = require('http').createServer(app); // Create a server
-const io = require('socket.io')(server, {
-    cors: {
-        origin: "*",
-        method: ["GET", "POST"]
-    }
-}); // Create a socket.io server
-const socketioJwt = require('socketio-jwt'); // Import socketio-jwt
-
-
-io.use(socketioJwt.authorize({
-    secret: process.env.SECRET,
-    handshake: true,
-    jwt: true,
-    auth_header_required: true
-}))
-
-
+const io = server.io;
 const socketRouter = require('./src/routes/SocketRouter')(io); // Import the SocketRouter
-
-
 
 app.use('/api', socketRouter); // Use the SocketRouter (api/
 
@@ -35,29 +16,3 @@ mongoose.connect(process.env.mongoURI, { useNewUrlParser: true, useUnifiedTopolo
 }).catch((err) => {
     console.log(`Error connecting to database ${err}`)
 });
-
-server.listen(3000, () => {
-    console.log("Server is running on port 3000");
-});
-
-// socket.io stuff
-io.on('connection', (socket) => {
-    const userId = socket.decoded_token.userId;
-    console.log(`User connected: ${userId}`);
-
-    // console.log('handshake: ', socket.handshake.auth)
-
-    socket.on('disconnect', () => {
-        console.log("User disconnected");
-    })
-
-    socket.onAny((event, ...args) => {
-        console.log(event, args)
-    })
-
-    socket.on("test", (playerName) => {
-        io.emit("test", playerName)
-    })
-});
-
-
---
+++ server.js
@@ -0,0 +1,49 @@
+"use strict"
+
+const http = require('http');
+const socketIO = require('socket.io');
+const socketioJwt = require('socketio-jwt');
+
+const app = require('./src/app');
+
+const server = http.createServer(app);
+const io = socketIO(server, {
+    cors: {
+        origin: "*",
+        method: ["GET", "POST"]
+    }
+});
+
+io.use(socketioJwt.authorize({
+    secret: process.env.SECRET,
+    handshake: true,
+    jwt: true,
+    auth_header_required: true
+}))
+
+server.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
+
+io.on('connection', (socket) => {
+    const userId = socket.decoded_token.userId;
+    console.log(`User connected: ${userId}`);
+
+    // console.log('handshake: ', socket.handshake.auth)
+
+    socket.on('disconnect', () => {
+        console.log("User disconnected");
+    })
+
+    socket.onAny((event, ...args) => {
+        console.log(event, args)
+    })
+
+    socket.on("test", (playerName) => {
+        io.emit("test", playerName)
+    })
+});
+
+module.exports = {
+    io: io
+}
